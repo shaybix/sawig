87cd25cbb92d507a732a91c04a9a89bb42127883
shaybix <shayba@shaybix.com>
2015-10-19 04:33:41 +0100

    added some more TODOs at controller package

--- controller/controller.go
+++ controller/controller.go
@@ -1,10 +1,27 @@
 package controller
 
+import (
+	"github.com/shaybix/sawig/bootstrap"
+)
 
-import "github.com/shaybix/sawig/bootstrap"
+// TODO: Need to load and parse the template filename, and return an error
+//
+//
+// Please look at PagesController to get a better idea as to what is the
+// desire result.
+//
 
-
+// Controller ...
 type Controller struct {
-    bootstrap.Controller
-    Page 
+	bootstrap.Controller
+	Page
 }
+
+// Templates ...
+type Templates struct {
+	FileName string
+	FilePath string
+}
+
+//ServeView ...
+func (t *Templates) ServeView(file string) {}
